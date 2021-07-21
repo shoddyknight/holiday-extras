2f18a301a2e9133610b01aa65f817885f0b0fde3
shoddyknight <timknight01@gmail.com>
2021-07-21 12:34:31 +0100

    Skeleton CR

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 const express = require('express')
 
+const users = require('./users')
+
+const port = process.env.PORT || 3000
+
 const app = express()
 
-const port = 3000
+app.use(express.json())
 
+
+/**
+ * Home to test express connectivity
+ */
 app.get('/', (req, res) => {
+  // TODO: return README or Swagger spec
   res.send('Hello Holiday Extras world!')
 })
 
+/**
+ * Setup express listener
+ */
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+/**
+ * Get a user
+ */
+app.get('/user/:userId', async (req, res) => {
+  console.log(`GET /user/:userId Request params: ${JSON.stringify(req.params)}`)
+  const {
+    params: {
+      userId = ''
+    } = {}
+  } = req
+
+  const user = await users.getUser(userId);
+
+  res.send(user)
+})
+
+/**
+ * Add a user
+ */
+app.post('/user', async (req, res) => {
+  console.log(`POST /user Request params: ${JSON.stringify(req.params)}`)
+
+  const createdUserId = await users.createUser(req.params)
+
+  res.send(createdUserId)
 })
---
+++ src/users.js
@@ -0,0 +1,21 @@
+
+const createUser = ({
+  email = '',
+  familyName = '',
+  givenName = ''
+} = {}) => {
+  // TODO: generate created time, add user to Db, return id
+
+  return 1
+}
+
+const getUser = (id) => {
+  // TODO: query Db for User
+
+  return {}
+}
+
+module.exports = {
+  createUser,
+  getUser
+}
